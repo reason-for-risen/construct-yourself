b2a1abfc72b655b5e935cc04afffc9d9d8e3bfcf
reason-for-risen <reason.for.risen@gmail.com>
2017-10-28 13:43:25 +0300

    Update Construction.hs

--- src/Construction.hs
+++ src/Construction.hs
@@ -1,12 +1,12 @@
 module Construction
   ( Name, Term(..)
   , bound, free, fresh
-  , reduce, substitute, alpha, beta, eta
+  , reduce, substitute, alpha, beta, eta, eq
   , termP, varP, appP, lamP, bracketP
   ) where
 
 import           Construction.Internal.Functions (alpha, beta, bound, eta, free,
-                                                  fresh, reduce, substitute)
+                                                  fresh, reduce, substitute, eq)
 import           Construction.Internal.Parser    (appP, bracketP, lamP, termP,
                                                   varP)
 import           Construction.Internal.Types     (Name, Term (..))
