507eb29c28b6c4f2227d0935f42bd80360866660
reason-for-risen <riserisen@yandex.ru>
2017-11-17 22:49:08 +0300

    up

--- src/Tasks/GADT_1/GADTParser.hs
+++ src/Tasks/GADT_1/GADTParser.hs
@@ -14,10 +14,12 @@ import           Text.Parsec.Text       (Parser)
 import           Text.Parsec.Token
 
 iLitP :: Parser (Lit Int)
-iLitP = undefined
+iLitP = try $ (ILit . read) <$>  spacedP (many1 digit) -- (\x -> bracketP x <|> x)
 
 bLitP :: Parser (Lit Bool)
-bLitP = undefined
+bLitP = let catchBool 'T' = BLit True
+            catchBool 'F' = BLit False
+        in try $ catchBool <$> spacedP (char 'T' <|> char 'F')
 
 iiLitP :: Parser (Expr Int)
 iiLitP = Lit <$> iLitP
@@ -26,24 +28,27 @@ bbLitP :: Parser (Expr Bool)
 bbLitP = Lit <$> bLitP
 
 addP :: Parser (Expr Int)
-addP = undefined
+addP = (Add <$> (spacedP (bracketP parse) <* char '+') <*> spacedP parse) <|>
+       (Add <$> (iiLitP <* char '+') <*> spacedP parse)
 
 leqP :: Parser (Expr Bool)
 leqP = Leq <$> (parse <* char '<') <*> parse
 
 andP :: Parser (Expr Bool)
-andP = undefined
+andP = (And <$> (spacedP (bracketP parse) <* string "&&") <*> spacedP parse) <|>
+       (And <$> (bbLitP <* string "&&") <*> spacedP parse)
 
 spacedP :: Parser a -> Parser a
 spacedP p = (many space *> p) <* many space
 
 bracketP :: Parser a -> Parser a
-bracketP = undefined
+bracketP = try . between (char '(') (char ')')
 
 class MyParse a where
   parse :: Parser (Expr a)
 
 instance MyParse Int where
-  parse = undefined
+  parse = try (spacedP addP) <|> try (bracketP parse) <|> iiLitP
 instance MyParse Bool where
-  parse = undefined
+  parse = try (spacedP leqP) <|> try (spacedP andP)
+          <|> try (bracketP parse) <|>  bbLitP
--- test/Task1Spec.hs
+++ test/Task1Spec.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
-    
+
 import           Test.Hspec
 import qualified Text.Parsec as TP (parse)
 import Text.Parsec.Text
-import Data.Text 
+import Data.Text
 import Tasks
 
 main :: IO ()
 main = hspec $
     describe "Task 1 test" parserTest
-    
+
 parserTest :: SpecWith ()
 parserTest = do
   it "#1" $ check parse "42" $ i 42
@@ -23,8 +23,8 @@ parserTest = do
   it "#7" $ check parse "42 < 41" $ i 42 `Leq` i 41
   it "#8" $ check parse "(42 < 41)" $ i 42 `Leq` i 41
   it "#9" $ check parse "T" $ b True
-  it "#10" $ check parse "F" $ b False 
-  it "#11" $ check parse "(F)" $ b False 
+  it "#10" $ check parse "F" $ b False
+  it "#11" $ check parse "(F)" $ b False
   it "#12" $ check parse "T && T" $ b True `And` b True
 
   it "#13" $ checkWithEval parse "T && (100 < 42)" $ BLit False
